c0258bd30426afba56adc5f5f8f4d240e9926bec
moichi <zzikbu@gmail.com>
2023-11-04 21:51:17 +0900

    Add BMI.xcodeproj

---
+++ 10 - BMI/BMI/SecondViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SecondViewController.swift
+//  BMI
+//
+//  Created by 이승민 on 11/4/23.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var bmiNumberLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    @IBOutlet weak var backButton: UIButton!
+    
+    var bmiNumber: Double?
+    var adviceString: String?
+    var bmiColor: UIColor?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        makeUI()
+        
+    }
+    
+    func makeUI() {
+        
+        bmiNumberLabel.clipsToBounds = true
+        bmiNumberLabel.layer.cornerRadius = 8
+        bmiNumberLabel.backgroundColor = .gray
+        
+        backButton.setTitle("다시 계산하기", for: .normal)
+        backButton.clipsToBounds = true
+        backButton.layer.cornerRadius = 8
+        
+        guard let bmi = bmiNumber else { return }
+        bmiNumberLabel.text = String(bmi)
+        
+        adviceLabel.text = adviceString
+        
+        bmiNumberLabel.backgroundColor = bmiColor
+    }
+
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
